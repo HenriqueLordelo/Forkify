cda7bf6f4d47ad0b25a7b01d6e20df790d855d67
Henrique Lordêlo <henriquelordelo742@gmail.com>
2024-09-23 19:10:28 -0300

    Aplicação final

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = `https://forkify-api.herokuapp.com/api/v2/recipes/`;
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = "7ebfcc6c-fd84-48ea-aaa6-f880dd649064";
+export const MODAL_CLOSE_SEC = 2.5;
